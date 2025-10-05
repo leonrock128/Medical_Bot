c153c1ead7a9506f1fba8f92aacbde0913ef0e59
leonrock128 <leonrock128@gmail.com>
2025-10-05 16:31:57 +0530

    Update Login page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -39,22 +39,22 @@ const Navbar = () => {
                         <div className='absolute top-0 right-0 pt-6 w-32 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                             <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
                                 <p onClick={()=> navigate('myprofile') } className='hover:text-black cursor-pointer'>My Profile</p>
-                                <p onClick={() => navigate('my-appointments')} className='hover:text-black cursor-pointer'>MY Appoinments</p>
+                                <p onClick={() => navigate('my-appointments')} className='hover:text-black cursor-pointer'>My Appoinments</p>
                                 <p onClick={() => setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
                             </div>   
                         </div>
                     </div>
-                    :<button onClick={() => navigate('/login')} className='bg-[#5f6fff] text-white px-8 py-3 rounded-full font-light hidden md:block'>
-                    Create account
-                </button>
+                    :<button onClick={() => navigate('/login')} className='bg-[#5f6fff] text-white px-8 py-3 rounded-full font-light hidden md:block cursor-pointer'>
+                        Create account
+                    </button>
                 }
-                <img onClick={()=> setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+                <img onClick={()=> setShowMenu(true)} className='w-6 md:hidden cursor-pointer' src={assets.menu_icon} alt="" />
 
                 {/* ----- Mobile Menu------ */}
                 <div className={`${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
                     <div className='flex items-center justify-between px-5 py-6'>
                         <img className='w-36' src={assets.logo} alt="" />
-                        <img className='w-7' onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="" />
+                        <img className='w-7 cursor-pointer' onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="" />
                     </div>
                     <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
                         <NavLink onClick={()=>setShowMenu(false)} to='/'><p className='px-4 py-2 rounded inline-block'>Home</p></NavLink>
