6fe94ba757d89f33d90e5f6c0565764e99de3e6c
leonrock128 <leonrock128@gmail.com>
2025-10-16 22:40:04 +0530

    updating Backend and Admin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,12 +10,14 @@ import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Footer from './components/Footer';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
   return (
     <div className='mx-4 sm:mx-[4%]'>
+      <ToastContainer />
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
