0fa11a3450461bf7f3bc5f143ae99fececa00e5a
leonrock128 <leonrock128@gmail.com>
2025-10-26 19:30:14 +0530

    Integrate payment and update admin appointment

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -3,8 +3,28 @@ import { createContext } from "react";
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
-    const value = {
 
+    const currency = "₹";
+
+    const calculateAge = (dob) => {
+        const today = new Date()
+        const birthDate = new Date(dob)
+
+        let age = today.getFullYear() - birthDate.getFullYear()
+        return age
+    }
+
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    
+      const slotDateFormat = (slotDate) => {
+        const dateObj = slotDate.split('-');
+        return dateObj[0] + ' ' + months[parseInt(dateObj[1]) - 1] + ', ' + dateObj[2];
+      }
+
+    const value = {
+        calculateAge,
+        slotDateFormat,
+        currency
     }
     return (
         <AppContext.Provider value={value}>
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { addDoctor,allDoctors,loginAdmin } from '../controllers/adminController.js'
+import { addDoctor,allDoctors,appointmentCancel,appointmentsAdmin,loginAdmin } from '../controllers/adminController.js'
 import upload from '../middlewares/multer.js'
 import authadmin from '../middlewares/authAdmin.js';
 import { changeAvailability } from '../controllers/doctorController.js'
+import authAdmin from '../middlewares/authAdmin.js';
 
 
 const adminRouter = express.Router()
@@ -11,6 +12,9 @@ adminRouter.post('/add-doctor',authadmin,upload.single('image'),addDoctor);
 adminRouter.post('/login',loginAdmin);
 adminRouter.post('/all-doctors',authadmin,allDoctors);
 adminRouter.post('/change-availability',authadmin,changeAvailability);
+adminRouter.get('/appointments',authAdmin,appointmentsAdmin)
+adminRouter.post('/cancel-appointment',authAdmin,appointmentCancel)
+
 
 // adminRouter.post("/upload", upload.single("image"), uploadImage);
 
